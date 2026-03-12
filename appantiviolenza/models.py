f4048437d25f283dbc93c908b0949f4930800f7c
Andruetto-Lorenzo <lorenzo.andruetto@istitutoagnelli.it>
2026-03-12 21:31:46 +0100

    fixed some stuff

--- appantiviolenza/models.py
+++ appantiviolenza/models.py
@@ -1,21 +1,5 @@
 from django.db import models
 
-class Utente(models.Model):
-    FASCE_ETA_CHOICES = [
-        ('-18', 'Minorenne'),
-        ('18-29', '18-29'),
-        ('30-49', '30-49'),
-        ('50-70', '50-70'),
-        ('70+', '70+'),
-    ]
-
-    id = models.BigAutoField(primary_key=True)
-    nome_utente = models.CharField(max_length=100, null=True, blank=True)
-    email = models.EmailField()
-    telefono = models.BigIntegerField(null=False, blank=False)
-    regione_provenienza = models.CharField(max_length=20, null=False, blank=False)
-    fascia_eta = models.CharField(max_length=10, choices=FASCE_ETA_CHOICES)
-
 class Operatore(models.Model):
     specializzazione = models.CharField(max_length=50, null=False, blank=True)
     id_operatore = models.BigAutoField(primary_key=True)
@@ -36,11 +20,30 @@ class Ticket(models.Model):
         ('Fake alarm', 'Falso allarme'),
     ]
 
-    data = models.DateField(null=False, blank=False)
-    ora = models.TimeField(null=False, blank=False)
+    LIVELLO_URGENZA_CHOICES = [
+        ('low', 'Basso'),
+        ('medium', 'Medio'),
+        ('high', 'Alto'),
+    ]
+
+    FASCE_ETA_CHOICES = [
+        ('-18', 'Minorenne'),
+        ('18-29', '18-29'),
+        ('30-49', '30-49'),
+        ('50-70', '50-70'),
+        ('70+', '70+'),
+    ]
+
+    nome_utente = models.CharField(max_length=100, null=True, blank=True)
+    email = models.EmailField()
+    telefono = models.BigIntegerField(null=False, blank=False)
+    regione_provenienza = models.CharField(max_length=20, null=False, blank=False)
+    fascia_eta = models.CharField(max_length=10, choices=FASCE_ETA_CHOICES)
+    data = models.DateField(null=False, blank=False, auto_now_add=True)
+    ora = models.TimeField(null=False, blank=False, auto_now_add=True)
     tipo_violenza = models.CharField(max_length=15, choices=VIOLENZA_CHOICES, null=False, blank=False)
     descrizione = models.CharField(max_length=500, null=False, blank=False)
-    livello_urgenza = models.IntegerField(null=False, blank=False)
+    livello_urgenza = models.IntegerField(null=False, blank=False, choices=LIVELLO_URGENZA_CHOICES)
     stato_richiesta = models.CharField(max_length=20, null=False, blank=False)
 
 class Segnalazioni(models.Model):
