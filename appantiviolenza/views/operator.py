901338d0767a1e184f6db07ae12c6d668d769835
Andruetto-Lorenzo <lorenzo.andruetto@istitutoagnelli.it>
2026-03-18 12:54:38 +0100

    done

--- appantiviolenza/views/operator.py
+++ appantiviolenza/views/operator.py
@@ -1,39 +1,10 @@
-# from django.shortcuts import render
-# from ..models import Ticket
-
-# def operator_view(request):
-#     tickets = Ticket.objects.all()
-#     # ticket.stato_richiesta = "In carico"
-#     # ticket.save()
-
-#     if request.method == 'POST':
-#         btn_accetta = request.POST.get("btn_accetta")
-#         if btn_accetta == 'Aperta':
-#             btn_accetta = 'In carico'
-#         else:
-#             btn_accetta = 'Chiusa'
-
-#         print(btn_accetta)
-
-#     print(tickets[0].stato_richiesta)
-
-#     return render(request, 'operator.html', {
-#         'tickets': tickets,
-#     })
-
-# def change_state_tickets(request):
-#     ticket = request.POST.get("btn_accetta")
-#     print(ticket)
-
-
 from django.shortcuts import render, get_object_or_404, redirect
 from django.conf import settings
 from ..models import Ticket
 import psycopg2
-import psycopg2.extras  # per RealDictCursor
- 
+import psycopg2.extras
+
 def _get_connection():
-    """Apre una connessione psycopg2 usando le stesse credenziali di settings.py."""
     db = settings.DATABASES['default']
     return psycopg2.connect(
         dbname=db['NAME'],
@@ -42,13 +13,13 @@ def _get_connection():
         host=db.get('HOST', 'localhost'),
         port=db.get('PORT', '5432'),
     )
- 
+
 def operator_view(request):
     if request.method == 'POST':
         ticket_id = request.POST.get("ticket_id")
         azione    = request.POST.get("azione")
         ticket    = get_object_or_404(Ticket, pk=ticket_id)
- 
+
         if azione == 'accetta' and ticket.stato_richiesta == 'Aperta':
             ticket.stato_richiesta = 'In carico'
             ticket.save()
@@ -58,17 +29,16 @@ def operator_view(request):
         elif azione == 'fake' and ticket.stato_richiesta == 'In carico':
             ticket.stato_richiesta = 'Fake alarm'
             ticket.save()
- 
-        return redirect('operator_space')
- 
+
+        ordina_corrente = request.POST.get('ordina', 'urgenza')
+        return redirect(f"/operator/?ordina={ordina_corrente}")
+
     ordina = request.GET.get('ordina', 'urgenza')
- 
+
     conn = _get_connection()
     try:
-        # RealDictCursor restituisce ogni riga come dizionario {colonna: valore}
-        # così nel template possiamo usare {{ t.nome_utente }}, {{ t.data }}, ecc.
         with conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor) as cur:
- 
+
             if ordina == 'data':
                 cur.execute("""
                     SELECT id, nome_utente, regione_provenienza, tipo_violenza,
@@ -93,11 +63,11 @@ def operator_view(request):
                         data DESC,
                         ora  DESC
                 """)
- 
+
             tickets = cur.fetchall()
     finally:
         conn.close()
- 
+
     return render(request, 'operator.html', {
         'tickets': tickets,
         'ordina':  ordina,
